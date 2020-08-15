8c8060948e85974836a51a341e5469faad1db5c6
Marta Ruiz <marta.ruiz@xing.com>
2020-08-15 13:33:10 +0200

    Fix linters

--- src/__tests__/run.test.ts
+++ src/__tests__/run.test.ts
@@ -1,4 +1,4 @@
-import {run} from '../src/run';
+import {run} from '../run';
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 import nock from 'nock';
@@ -9,6 +9,7 @@ beforeEach(() => {
 
   github.context.payload = {
     action: 'opened',
+    // eslint-disable-next-line @typescript-eslint/camelcase
     pull_request: {
       number: 1
     }
@@ -26,6 +27,7 @@ test('comments on PR', async () => {
       body => body.body === 'Test Comment'
     )
     .reply(200, {
+      // eslint-disable-next-line @typescript-eslint/camelcase
       html_url: 'https://github.com/testowner/testrepo/issues/1#issuecomment-1'
     });
   const setOutputMock = jest.spyOn(core, 'setOutput');
--- src/run.ts
+++ src/run.ts
@@ -4,7 +4,7 @@ import * as github from '@actions/github';
 export async function run(): Promise<void> {
   try {
     const comment = core.getInput('comment');
-    const github_token = core.getInput('GITHUB_TOKEN');
+    const githubToken = core.getInput('GITHUB_TOKEN');
 
     const context = github.context;
     if (context.payload.pull_request == null) {
@@ -12,12 +12,13 @@ export async function run(): Promise<void> {
       return;
     }
 
-    const pull_request_number = context.payload.pull_request.number;
+    const pullRequestNumber = context.payload.pull_request.number;
 
-    const octokit = github.getOctokit(github_token);
+    const octokit = github.getOctokit(githubToken);
     const response = await octokit.issues.createComment({
       ...context.repo,
-      issue_number: pull_request_number,
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      issue_number: pullRequestNumber,
       body: comment
     });
 
